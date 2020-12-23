2f1d02b7efad034be609e638e4575427b30015ad
xw@bctos.cn <xw@bctos.cn>
2020-12-23 19:37:41 +0800

    更新默认的PHP容器版本

--- panel-install.sh
+++ panel-install.sh
@@ -277,7 +277,7 @@ fi
 tips "启用常用的nginx+php+mysql这三个容器";
 cd /bctos/server
 names=$(docker ps -a --format '{{.Names}}')
-[ -z $(echo $names|grep 'php'|sed 's/ //g') ] && docker-compose -f php72/docker-compose.yml up -d
+[ -z $(echo $names|grep 'php'|sed 's/ //g') ] && docker-compose -f php74/docker-compose.yml up -d
 [ -z $(echo $names|grep 'mysql'|sed 's/ //g') ] && docker-compose -f mysql57/docker-compose.yml up -d
 [ -z $(echo $names|grep 'nginx'|sed 's/ //g') ] && docker-compose -f nginx/docker-compose.yml up -d
 
--- wwwroot/bctos.cn/scripts/sys/initWebDocker.sh
+++ wwwroot/bctos.cn/scripts/sys/initWebDocker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /bctos/server
 names=$(docker ps -a --format '{{.Names}}')
-[ -z $(echo $names|grep 'php'|sed 's/ //g') ] && docker-compose -f php72/docker-compose.yml up -d
+[ -z $(echo $names|grep 'php'|sed 's/ //g') ] && docker-compose -f php74/docker-compose.yml up -d
 [ -z $(echo $names|grep 'mysql'|sed 's/ //g') ] && docker-compose -f mysql57/docker-compose.yml up -d
 [ -z $(echo $names|grep 'nginx'|sed 's/ //g') ] && docker-compose -f nginx/docker-compose.yml up -d
 
